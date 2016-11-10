8cbdc91ceb534679bd4fd7897ccf989a9450d067
Ped90 <Ped90@Ped90>
2016-11-10 09:02:36 +0100

    commit de inicio

---
+++ examped/src/examped/Examped.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examped;
+
+/**
+ *
+ * @author Ped90
+ */
+public class Examped {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //  realiza commits (1 punto) y rama con el nombre 'exp' (1 punto) 
+        //según el esquema y orden de las letras. Al final tiene que haber un merge
+        
+        System.out.println("Hola");
+    }
+    
+}
